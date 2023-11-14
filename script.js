38e6fecd6b620231b99d1d01390435e67cd2f46e
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-14 10:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const extendHex = (shortHex) => {
-  // write your code here
+   const hexWithoutHash = shortHex.replace(/^#/, '').toLowerCase();
+
+  // Ensure the input is a valid short hex code
+  if (!/^[0-9a-f]{3}$/i.test(hexWithoutHash)) {
+    console.error('Invalid short hex code');
+    return null;
+  }
+
+  // Expand short hex code to full hex code
+  const fullHex = hexWithoutHash
+    .split('')
+    .map(component => component.repeat(2))
+    .join('');
+
+  // Add the # prefix and return the result
+  return `#${fullHex}`;
 };
 
 // Do not change the code below.
